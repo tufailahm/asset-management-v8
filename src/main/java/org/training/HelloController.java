b3b32fe7bd6002564d2147bf9e0101d6b8b18e1c
Mohammad Tufail Ahmed <Tufail Ahmed>
2026-09-16 10:21:16 +0530

    first commit done by tufail

---
+++ src/main/java/org/training/HelloController.java
@@ -0,0 +1,13 @@
+package org.training;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+    @RequestMapping("hello")    //localhost:7072/hello?username=riya
+    public String hello(@RequestParam("username")String username ){
+        return "Hello : "+username + "  Current Date and time is :"+new java.util.Date();
+    }
+}
---
+++ src/test/java/org/training/AssetManagementV8ApplicationTests.java
@@ -0,0 +1,13 @@
+package org.training;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AssetManagementV8ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
